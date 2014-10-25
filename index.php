05e81fca2f3c891c043c17df678cca2852e3b62c
Vladimir Gavrylov <iboved@gmail.com>
2014-10-25 14:47:29 +0300

    Changed notifications in User.php

--- index.php
+++ index.php
@@ -6,12 +6,14 @@ use Core\User;
 use Core\Support;
 use Core\Admin;
 
-$User = new User("Pavel", "Lavin", "Male", "18");
+$User = new User("Pavel", "Bedford", "Male", "18");
 $Support = new Support("Alina", "Naumova", "Female", "24");
 $Admin = new Admin("Vova", "Gavrylov", "Male", "21");
 
 $User->editFirstName("Armin");
 $User->editLastName("Buuren");
+$User->editAge("28");
+$User->editSex("Female");
 
 $Support->ban($User);
 $Support->unban($User);
